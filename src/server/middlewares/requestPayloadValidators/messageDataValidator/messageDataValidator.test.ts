2a71c718bd224a61801142ac3c531d90cf49aea1
Pau Ibáñez <pauibanez2001@gmail.com>
2023-09-28 15:43:38 +0200

    added test for invalid payload

--- src/server/middlewares/requestPayloadValidators/messageDataValidator/messageDataValidator.test.ts
+++ src/server/middlewares/requestPayloadValidators/messageDataValidator/messageDataValidator.test.ts
@@ -1,5 +1,8 @@
 import messageDataValidator from "./messageDataValidator";
-import { validMessagePayload } from "./messageDataValidator.testObjects";
+import {
+  missingNameMessagePayload,
+  validMessagePayload,
+} from "./messageDataValidator.testObjects";
 
 describe("Given messageDataValidator", () => {
   describe("When it's passed a valid payload", () => {
@@ -14,6 +17,21 @@ describe("Given messageDataValidator", () => {
 
       expect(next).toHaveBeenCalledWith();
       expect(req.body).toEqual(validMessagePayload);
+    });
+  });
+
+  describe("When it's passed a payload with missing firstName", () => {
+    test("Then it should call next an error", () => {
+      const req: any = {
+        body: missingNameMessagePayload,
+      };
+
+      const next = jest.fn();
+
+      messageDataValidator(req, null, next);
+
+      expect(next).toHaveBeenCalled();
+      expect(next).not.toHaveBeenCalledWith();
     });
   });
 });
--- src/server/middlewares/requestPayloadValidators/messageDataValidator/messageDataValidator.testObjects.ts
+++ src/server/middlewares/requestPayloadValidators/messageDataValidator/messageDataValidator.testObjects.ts
@@ -9,3 +9,12 @@ export const validMessagePayload: MessageData = {
   subject: "subject",
   message: "message",
 };
+
+export const missingNameMessagePayload = {
+  lastName: "name",
+  messageType: MessageType.JobOportunity,
+  typeVariable: "company",
+  email: "email@mail.com",
+  subject: "subject",
+  message: "message",
+};
