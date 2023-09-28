00e9c0f6fb66cbb368ca384056497c13a410eead
Pau Ibáñez <pauibanez2001@gmail.com>
2023-09-28 15:44:11 +0200

    removed TOKEN_SECRET mock

--- jest.setup.ts
+++ jest.setup.ts
@@ -7,7 +7,6 @@ const originalEnv = { ...process.env };
 beforeEach(() => {
   jest.resetModules();
   process.env = { ...originalEnv };
-  process.env.TOKEN_SECRET = "Super secure secret";
 });
 
 afterEach(() => {
