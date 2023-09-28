a0acd8c0164754a6c8e35c94d869e76bbedea609
Pau Ibáñez <pauibanez2001@gmail.com>
2023-09-28 14:34:13 +0200

    added validator content

--- src/server/middlewares/requestPayloadValidators/messageDataValidator/messageDataValidator.ts
+++ src/server/middlewares/requestPayloadValidators/messageDataValidator/messageDataValidator.ts
@@ -4,5 +4,27 @@ const messageDataValidator = (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  // Options to tell Joi how to validate the payload
+  const joiValidationOptions = {
+    abortEarly: false,
+    allowUnknown: true,
+    stripUnknown: true,
+  };
+
+  const { error, value } = loginData.validate(req.body, joiValidationOptions); // Use the loginData Joi object to validate the request body. (This object is found in data/JoiObjects)
+
+  // If the validation fails write an error and go next.
+  if (error) {
+    const invalidLoginDataError = getInvalidLoginDataError(
+      error.details.map((detail) => detail.message)
+    );
+    next(invalidLoginDataError);
+    return;
+  }
+
+  // If the validation is correct we override the request body with the validated object and go next.
+  req.body = value;
+  next();
+};
 export default messageDataValidator;
