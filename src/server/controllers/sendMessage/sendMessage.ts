fb85fd26a300539853adc39fc7aba4dd69835c6a
Pau Ibáñez <pauibanez2001@gmail.com>
2023-09-28 15:58:52 +0200

    added the sendMessage content

--- src/server/controllers/sendMessage/sendMessage.ts
+++ src/server/controllers/sendMessage/sendMessage.ts
@@ -1,16 +1,37 @@
 import { NextFunction, Request, Response } from "express";
 import EmailData from "../../utils/email/types";
 import MessageData from "../../../types/MessageData";
+import getMessageReceivedEmail from "../../utils/email/emailBuilders/messageReceivedEmail";
+import getAwaitingAnswerEmail from "../../utils/email/emailBuilders/awaitingAnswerEmail";
+import sendEmail from "../../utils/email";
 
-const sendMessage = (req: Request, res: Response, next: NextFunction) => {
-  const messge = req.body as MessageData;
+const sendMessage = async (req: Request, res: Response, next: NextFunction) => {
+  const message = req.body as MessageData;
 
   const toSenderEmailData: EmailData = {
-    subject: `Hello ${messge.firstName}! thanks for your email.`,
-    to: messge.email,
+    subject: `Hello ${message.firstName}! thanks for your email.`,
+    to: message.email,
     internalEmailName: "Contact soon",
-    html: getMessageReceivedEmail(messge.firstName),
+    html: getMessageReceivedEmail(message.firstName),
   };
+
+  const toMeEmailData: EmailData = {
+    subject: message.subject,
+    to: "pauibanez2001@gmail.com",
+    internalEmailName: `Awaiting answer from ${message.firstName}`,
+    html: getAwaitingAnswerEmail(message),
+  };
+
+  try {
+    await sendEmail(toSenderEmailData);
+    await sendEmail(toMeEmailData);
+  } catch (error) {
+    next(error);
+  }
+
+  res.status(200).json({
+    message: "Message received!",
+  });
 };
 
 export default sendMessage;
