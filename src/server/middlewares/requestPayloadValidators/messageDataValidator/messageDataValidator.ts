21da587924a024dc64448632b084b36b2e413ced
Pau Ibáñez <pauibanez2001@gmail.com>
2023-09-28 14:33:50 +0200

    created the middleware

---
+++ src/server/middlewares/requestPayloadValidators/messageDataValidator/messageDataValidator.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from "express";
+
+const messageDataValidator = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
+export default messageDataValidator;
