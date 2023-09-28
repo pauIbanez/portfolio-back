375b5e2996ed9f8e6bd071f6fa9c0697df2fff38
Pau Ibáñez <pauibanez2001@gmail.com>
2023-09-28 15:49:04 +0200

    created the messageReceived email builder

---
+++ src/server/utils/email/emailBuilders/messageReceivedEmail.ts
@@ -0,0 +1,7 @@
+const messageReceivedEmail = (name: string) => `
+  Thank you for your message ${name}!.
+
+  I've received your message and I'll asnwer as soon as posible.
+`;
+
+export default messageReceivedEmail;
